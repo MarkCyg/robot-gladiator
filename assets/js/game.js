801cd67b234d23d62fb6bdf30c82d6e524f12222
MarkCyg <87244359+MarkCyg@users.noreply.github.com>
2021-09-29 15:09:26 -0400

    added var and functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("this is an alert! Javascript is running!")
+var playerName = window.prompt("What is you robot's name?");
+
+//what is this?
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+//this will do math
+console.log(10 + 10);
+//what is this?
+console.log("our robot's name is" + playerName);
+
+// note the lack of quotations around playerName
+window.alert(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("the fight has begun");
+}
+
+// fight();
